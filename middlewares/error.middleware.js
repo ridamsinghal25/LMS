8f68f7ff822e77ba68d22dbd05a5e73b34195bb2
ridamsinghal25 <ridamsinghal25@gmail.com>
2025-01-15 14:56:57 +0530

    add middleware file to codebase

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,16 @@
+export class ApiError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true; // Optional
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export const catchAsync = (fn) => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
